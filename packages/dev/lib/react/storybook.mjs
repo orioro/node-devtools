e313e2d636d8f042b281785647159c771c6a2ac9
Simon Fan <simon@orioro.design>
2024-03-13 22:11:35 -0300

    feat: add FlexGrid component

--- packages/dev/lib/react/storybook.mjs
+++ packages/dev/lib/react/storybook.mjs
@@ -1,10 +1,12 @@
 import { join, dirname } from 'path'
+import { createRequire } from 'node:module'
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
+  const require = createRequire(import.meta.url)
   return dirname(require.resolve(join(value, 'package.json')))
 }
 
