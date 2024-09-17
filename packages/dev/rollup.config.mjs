c3045353e7325723c56ef32228194d71d2e36055
Simon Fan <simon@orioro.design>
2024-09-16 21:27:34 -0300

    feat: improve lib integration

--- packages/dev/rollup.config.mjs
+++ packages/dev/rollup.config.mjs
@@ -1,7 +1,41 @@
-import { rollupConfig } from './lib/ts/index.mjs'
+import { basename, dirname, extname, join } from 'node:path'
+import { rollupConfig } from './src/ts/index.mjs'
 
-export default rollupConfig((base) => ({
-  ...base,
-  input: './lib/index.mjs',
-  plugins: [...base.plugins],
-}))
+function replaceExtension(relativePath, newExt) {
+  const dir = dirname(relativePath) // Get the directory name
+  const baseName = basename(relativePath, extname(relativePath)) // Get the base name without the current extension
+  return join(dir, `${baseName}${newExt}`) // Join the directory and new file name with new extension
+}
+
+function scriptPaths(relativePath) {
+  return {
+    input: `./src/${relativePath}`,
+    output: [
+      {
+        file: `dist/${replaceExtension(relativePath, '.mjs')}`,
+        format: 'esm',
+      },
+      {
+        file: `dist/${replaceExtension(relativePath, '.cjs')}`,
+        format: 'cjs',
+      },
+    ],
+  }
+}
+
+const scripts = [
+  rollupConfig((base) => ({
+    ...base,
+    ...scriptPaths('index.mjs'),
+  })),
+  rollupConfig((base) => ({
+    ...base,
+    ...scriptPaths('react/index.mjs'),
+  })),
+  rollupConfig((base) => ({
+    ...base,
+    ...scriptPaths('ts/index.mjs'),
+  })),
+]
+
+export default Promise.all(scripts)
